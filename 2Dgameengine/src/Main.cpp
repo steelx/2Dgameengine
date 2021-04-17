992150d8901b4ec096984e50e22acfa7d8166d70
Ajinkya <ajinkyab1987@gmail.com>
2021-04-17 18:02:09 +0530

    Game obj moved to STACK

--- 2Dgameengine/src/Main.cpp
+++ 2Dgameengine/src/Main.cpp
@@ -3,18 +3,18 @@
 #include "Constants.h"
 
 int main(int argc, char* argv[]) {
-    Game* game = new Game();
+    Game game;
 
-    game->Initialize(WINDOW_WIDTH, WINDOW_HEIGHT);
+    game.Initialize(WINDOW_WIDTH, WINDOW_HEIGHT);
 
-    while (game->IsRunning())
+    while (game.IsRunning())
     {
-        game->ProcessInput();
-        game->Update();
-        game->Render();
+        game.ProcessInput();
+        game.Update();
+        game.Render();
     }
 
-    game->Destroy();
+    game.Destroy();
 
     return 0;
 }
